3684e6144382a68f50a09b7cc4969467b3e81edf
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-12 22:12:44 +0200

    Create maven project

---
+++ src/main/java/de/allianz/BGBGBGBGBGBGB/BgbgbgbgbgbgbApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.BGBGBGBGBGBGB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgbgbgbgbgbgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgbgbgbgbgbgbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/BGBGBGBGBGBGB/BgbgbgbgbgbgbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.BGBGBGBGBGBGB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgbgbgbgbgbgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
